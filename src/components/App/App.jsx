7ff23c973091434402bee24a4835dfa68fcb5f38
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-05-18 16:38:04 +0300

    refactored App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Searchbar } from '../Searchbar/Searchbar';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
 import { Button } from '../Button/Button';
@@ -8,91 +8,79 @@ import { Modal } from '../Modal/Modal';
 
 import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    galleryItems: [],
-    totalItems: 0,
-    isLoading: false,
-    showModal: false,
-    modalImg: {
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [galleryItems, setGalleryItems] = useState([]);
+  const [totalItems, setTotalItems] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImg, setModalImg] = useState({
+    url: '',
+    alt: '',
+  });
+
+  useEffect(() => {
+    if (query !== '') {
+      fetchData();
+    }
+  }, [query, page]);
+
+  async function fetchData() {
+    try {
+      setIsLoading(true);
+      const data = await getSearchGallery(query, page);
+      setGalleryItems([...galleryItems, ...data.hits]);
+      setTotalItems(data.totalHits);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const handleSearch = searchText => {
+    if (query === searchText) {
+      return alert(`You are alredy watching "${searchText}" category`);
+    }
+
+    setQuery(searchText.toLowerCase());
+    setGalleryItems([]);
+    setPage(1);
+  };
+
+  const handleLoadmoreImages = () => {
+    setPage(page + 1);
+  };
+
+  const openModal = item => {
+    setShowModal(true);
+    setModalImg({
+      url: item.largeImageURL,
+      alt: item.tags,
+    });
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setModalImg({
       url: '',
       alt: '',
-    },
-  };
-
-  async componentDidUpdate(_, prevState) {
-    const { query, page, galleryItems } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      try {
-        this.setState({ isLoading: true });
-        const data = await getSearchGallery(query, page);
-        this.setState({
-          galleryItems: [...galleryItems, ...data.hits],
-          totalItems: data.totalHits,
-        });
-      } catch (error) {
-        this.setState({ error });
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
-  }
-
-  handleSearch = query => {
-    if (this.state.query === query) {
-      return alert(`You are alredy watching "${query}" category`);
-    }
-
-    this.setState({
-      query: query.toLowerCase(),
-      galleryItems: [],
-      page: 1,
     });
   };
 
-  handleLoadmoreImages = () => {
-    this.setState({
-      page: this.state.page + 1,
-    });
-  };
-
-  openModal = item => {
-    this.setState(prevState => ({
-      showModal: true,
-      modalImg: {
-        url: item.largeImageURL,
-        alt: item.tags,
-      },
-    }));
-  };
-
-  closeModal = () => {
-    this.setState(prevState => ({
-      showModal: false,
-      modalImg: {},
-    }));
-  };
-
-  render() {
-    const { galleryItems, totalItems, isLoading, showModal, modalImg } =
-      this.state;
-    return (
-      <div className={css.App}>
-        <Searchbar onSubmit={this.handleSearch}></Searchbar>
-        <ImageGallery
-          galleryItems={galleryItems}
-          openModal={this.openModal}
-        ></ImageGallery>
-        {isLoading && <Loader />}
-        {totalItems > galleryItems.length && (
-          <Button onClick={this.handleLoadmoreImages}></Button>
-        )}
-        {showModal && (
-          <Modal modalImg={modalImg} closeModal={this.closeModal} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={handleSearch}></Searchbar>
+      <ImageGallery
+        galleryItems={galleryItems}
+        openModal={openModal}
+      ></ImageGallery>
+      {isLoading && <Loader />}
+      {totalItems > galleryItems.length && (
+        <Button onClick={handleLoadmoreImages}></Button>
+      )}
+      {showModal && <Modal modalImg={modalImg} closeModal={closeModal} />}
+    </div>
+  );
+};
